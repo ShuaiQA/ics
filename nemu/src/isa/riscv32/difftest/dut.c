d870ec94361a5b8c7c5dbff0fcf8cbcd69ab096a
tracer-ics2022 <tracer@njuics.org>
2023-04-25 16:06:32 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:06:32 up  2:48,  5 users,  load average: 0.65, 0.47, 0.40

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -1,25 +1,36 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
-#include <isa.h>
-#include <cpu/difftest.h>
 #include "../local-include/reg.h"
+#include <cpu/difftest.h>
+#include <isa.h>
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  bool ans = true;
+  if (ref_r->pc != cpu.pc) {
+    printf("pc is 0x%x fact is 0x%x", ref_r->pc, cpu.pc);
+    ans = false;
+  }
+  for (int i = 0; i < 32; i = i + 1) {
+    if (ref_r->gpr[i] != cpu.gpr[i]) {
+      printf("reg %d expect is 0x%x fact is 0x%x\n", i, ref_r->gpr[i],
+             cpu.gpr[i]);
+      ans = false;
+    }
+  }
+  return ans;
 }
 
-void isa_difftest_attach() {
-}
+void isa_difftest_attach() {}
