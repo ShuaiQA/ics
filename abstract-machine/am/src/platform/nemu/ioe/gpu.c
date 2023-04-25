9a83617be11b4898c3aa07e5881b1efb6d13a72a
tracer-ics2022 <tracer@njuics.org>
2023-04-25 19:04:56 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:04:56 up  5:46,  6 users,  load average: 0.24, 0.33, 0.30

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,23 +3,31 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
-void __am_gpu_init() {
-}
+void __am_gpu_init() {}
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+  uint32_t f1 = inl(VGACTL_ADDR);
+  uint32_t heigh = f1 & 0xffff;
+  uint32_t width = f1 >> 16;
+  *cfg = (AM_GPU_CONFIG_T){.present = true,
+                           .has_accel = false,
+                           .width = width,
+                           .height = heigh,
+                           .vmemsz = 0};
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *p = (uint32_t *)ctl->pixels; // FB_ADDR
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  int cnt = 0;
+  for (int j = ctl->y; j < ctl->y + ctl->h; j++) {
+    for (int i = ctl->x; i < ctl->x + ctl->w; i++) {
+      fb[i + j * 400] = p[cnt++];
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
 }
 
-void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
-}
+void __am_gpu_status(AM_GPU_STATUS_T *status) { status->ready = true; }
