d1ca78ff66c7a327a80c3595f82e4dba934b8aa0
tracer-ics2022 <tracer@njuics.org>
2023-04-25 14:04:50 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:04:50 up 46 min,  3 users,  load average: 0.36, 0.38, 0.37

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,28 +1,121 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  va_list args;
+  va_start(args, fmt);
+  char buf[10240];
+  int n = vsnprintf(buf, 10240, fmt, args);
+  va_end(args);
+  for (int i = 0; buf[i] != '\0'; i++) {
+    putch(buf[i]);
+  }
+  return n;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int num = vsnprintf(out, 100000, fmt, ap);
+  va_end(ap);
+  return num;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  int num = vsnprintf(out, 100000, fmt, ap);
+  return num;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int num = vsnprintf(out, n, fmt, ap);
+  va_end(ap);
+  return num;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  int nn = 0;
+  int num = 0;
+  uint32_t temp;
+  char *a;
+  char t[32];
+  int tt = 0;
+  int i = 0;
+  while (n-- && *fmt) {
+    if (*fmt != '%') {
+      *out++ = *fmt++;
+      num++;
+      continue;
+    }
+    // 当前是%
+    switch (*++fmt) {
+    case 's':
+      a = va_arg(ap, char *);
+      for (int i = 0; a[i] != '\0'; i++) {
+        num++;
+        *out++ = a[i];
+      }
+      tt = 1; // 标记当前%s
+      break;
+    case 'x':
+    case 'p':
+      temp = va_arg(ap, uint32_t);
+      i = 0;
+      while (temp != 0) {
+        int yu = temp % 16;
+        if (yu < 10) {
+          t[i] = '0' + yu;
+        } else {
+          t[i] = 'a' + yu - 10;
+        }
+        temp /= 16;
+        i++;
+      }
+      if (i == 0) {
+        *out++ = '0';
+      }
+      for (int j = i - 1; j >= 0; j--) {
+        *out++ = t[j];
+        num++;
+      }
+      tt = 1;
+      break;
+    case 'd':
+      nn = va_arg(ap, int);
+      if (nn < 0) {
+        *out++ = '-';
+        nn = -nn;
+      }
+      i = 0;
+      while (nn != 0) {
+        t[i] = '0' + (nn % 10);
+        nn /= 10;
+        i++;
+      }
+      if (i == 0) {
+        *out++ = '0';
+      }
+      for (int j = i - 1; j >= 0; j--) {
+        *out++ = t[j];
+        num++;
+      }
+      tt = 1;
+      break;
+    }
+    if (tt == 0) { // 当前不是
+      *out++ = '%';
+      *out++ = *fmt;
+      num += 2;
+    }
+    fmt++;
+  }
+  *out = '\0';
+  return num;
 }
 
 #endif
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,47 +1,134 @@
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdint.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t len = 0;
+  for (; *s != '\0'; s++) {
+    len++;
+  }
+  return len;
 }
 
+// 返回的是一个dst最初的位置
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *p = dst;
+  while (*src != '\0') {
+    *p++ = *src++;
+  }
+  *p = '\0';
+  return dst;
 }
 
+// 返回的是一个dst最初的位置
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  char *p = dst;
+  for (size_t i = 0; i < n; i++) {
+    *p++ = *src++;
+  }
+  return dst;
 }
 
+// 把 src 所指向的字符串追加到 dest 所指向的字符串的结尾
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *p = dst;
+  p = p + strlen(p);
+  strcpy(p, src);
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  while (*s1 != '\0' && *s2 != '\0') {
+    if (*s1 > *s2) {
+      return 1;
+    } else if (*s1 < *s2) {
+      return -1;
+    }
+    s1++;
+    s2++;
+  }
+  if (*s1 == '\0' && *s2 == '\0') {
+    return 0;
+  } else if (*s1 == '\0') {
+    return -1;
+  } else {
+    return 1;
+  }
 }
 
+// 比较到\0就结束了
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  for (int i = 0; i < n; i++) {
+    if (*s1 == '\0' || *s2 == '\0') {
+      break;
+    }
+    if (*s1 > *s2) {
+      return 1;
+    } else if (*s1 < *s2) {
+      return -1;
+    }
+    s1++;
+    s2++;
+  }
+  if (*s1 == '\0' && *s2 == '\0') {
+    return 0;
+  } else if (*s1 == '\0') {
+    return -1;
+  } else {
+    return 1;
+  }
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  unsigned char *p = (unsigned char *)s;
+  while (n > 0) {
+    *p++ = (unsigned char)c;
+    n--;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  unsigned char *d = (unsigned char *)dst;
+  unsigned char *s = (unsigned char *)src;
+  if (s + n > d && d > s) { // 发生重叠反向拷贝
+    while (n--) {
+      *(d + n) = *(s + n);
+    }
+  } else {
+    while (n--) {
+      *d++ = *s++;
+    }
+  }
+  return dst;
 }
 
+// 不支持in和out有重叠区域
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  unsigned char *o = (unsigned char *)out;
+  unsigned char *i = (unsigned char *)in;
+  for (size_t ii = 0; ii < n; ii++) {
+    *o++ = *i++;
+  }
+  return out;
 }
 
+// 按照n个进行比较即使是到\0
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  unsigned char *b1 = (unsigned char *)s1;
+  unsigned char *b2 = (unsigned char *)s2;
+  while (n--) {
+    if (*b1 > *b2) {
+      return 1;
+    } else if (*b1 < *b2) {
+      return -1;
+    }
+    b1++;
+    b2++;
+  }
+  return 0;
 }
 
 #endif
