eab8ed6d0b28c707db6257decfd57f893bde6c91
tracer-ics2022 <tracer@njuics.org>
2023-04-25 20:44:02 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:44:02 up  7:26,  3 users,  load average: 0.12, 0.35, 0.35

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #ifndef __ISA_RISCV32_H__
 #define __ISA_RISCV32_H__
@@ -21,6 +21,10 @@
 typedef struct {
   word_t gpr[32];
   vaddr_t pc;
+  vaddr_t mtvec;  // 存放异常处理入口地址
+  word_t mepc;    // 存放触发异常的PC
+  word_t mcause;  // 存放触发异常号
+  word_t mstatus; // 存放处理器的状态
 } riscv32_CPU_state;
 
 // decode
