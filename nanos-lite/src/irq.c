a6e2d601e24f5a5316f44c6a49e01204d5d9a4fe
tracer-ics2022 <tracer@njuics.org>
2023-04-26 17:10:21 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:10:21 up 1 day,  7:24,  5 users,  load average: 0.21, 0.18, 0.18

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,6 +1,17 @@
 #include "am.h"
+#include "arch/riscv32-nemu.h"
 #include <common.h>
 #include <stdio.h>
+
+void sys_write(Context *c, int fd, void *buf, int count) {
+  char *b = (char *)buf;
+  int i = 0;
+  for (; i < count; i++) {
+    char t = *(b + i);
+    putch(t);
+  }
+  c->GPRx = i;
+}
 
 static Context *do_event(Event e, Context *c) {
   switch (e.event) {
