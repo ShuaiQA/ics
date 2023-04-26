d66ac097af0ab035bacbb2efd679f4da7ec0aa1f
tracer-ics2022 <tracer@njuics.org>
2023-04-26 16:50:44 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:50:44 up 1 day,  7:04,  5 users,  load average: 0.21, 0.19, 0.18

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -1,14 +1,14 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
   write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
   while (1) {
-    j ++;
+    j++;
     if (j == 10000) {
-      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+      printf("Hello World from Navy-apps for the %dth time!\n", i++);
       j = 0;
     }
   }
