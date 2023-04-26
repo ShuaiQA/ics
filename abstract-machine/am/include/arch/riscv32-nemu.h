b84c1d0e35734d2289268cb4257adc2d7103d0e0
tracer-ics2022 <tracer@njuics.org>
2023-04-26 17:49:34 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:49:34 up 1 day,  8:03,  6 users,  load average: 0.64, 0.42, 0.25

--- abstract-machine/am/include/arch/riscv32-nemu.h
+++ abstract-machine/am/include/arch/riscv32-nemu.h
@@ -8,10 +8,10 @@ struct Context {
 };
 
 #define GPR1 gpr[17] // a7
-#define GPR2 gpr[5]
-#define GPR3 gpr[6]
-#define GPR4 gpr[7]
-#define GPRx gpr[5]
+#define GPR2 gpr[10]
+#define GPR3 gpr[11]
+#define GPR4 gpr[12]
+#define GPRx gpr[10]
 
 // intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
 //   register intptr_t _gpr1 asm (GPR1) = type;
