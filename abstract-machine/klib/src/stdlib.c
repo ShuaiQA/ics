e0da559666c3a1da7eaad31baa0f7fb68be2fac5
tracer-ics2022 <tracer@njuics.org>
2023-04-26 11:46:38 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:46:38 up 1 day,  2:00,  7 users,  load average: 1.26, 0.52, 0.35

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -1,6 +1,6 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
@@ -8,38 +8,45 @@ static unsigned long int next = 1;
 int rand(void) {
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
-  return (unsigned int)(next/65536) % 32768;
+  return (unsigned int)(next / 65536) % 32768;
 }
 
-void srand(unsigned int seed) {
-  next = seed;
-}
+void srand(unsigned int seed) { next = seed; }
 
-int abs(int x) {
-  return (x < 0 ? -x : x);
-}
+int abs(int x) { return (x < 0 ? -x : x); }
 
-int atoi(const char* nptr) {
+int atoi(const char *nptr) {
   int x = 0;
-  while (*nptr == ' ') { nptr ++; }
+  while (*nptr == ' ') {
+    nptr++;
+  }
   while (*nptr >= '0' && *nptr <= '9') {
     x = x * 10 + *nptr - '0';
-    nptr ++;
+    nptr++;
   }
   return x;
 }
+
+static char *mem_heap;
+static char *mem_brk;
 
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
-#endif
-  return NULL;
+  // #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+  //   panic("Not implemented");
+  // #endif
+  if (mem_heap == NULL) {
+    mem_heap = (char *)ROUNDUP(heap.start, 8);
+    mem_brk = mem_heap;
+    return (void *)mem_brk;
+  }
+  char *old_brk = mem_brk;
+  mem_brk += size;
+  return (void *)old_brk;
 }
 
-void free(void *ptr) {
-}
+void free(void *ptr) {}
 
 #endif
