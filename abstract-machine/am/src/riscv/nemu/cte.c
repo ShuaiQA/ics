9d83d01ddee56e0e115c03f78e02d1e741203184
tracer-ics2022 <tracer@njuics.org>
2023-04-30 11:21:40 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:21:40 up 3 days, 11:32,  6 users,  load average: 0.28, 0.26, 0.27

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -60,7 +60,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 }
 
 // yield的a7应该是1吧
-void yield() { asm volatile("li a7, 1; ecall"); }
+void yield() { asm volatile("li a7, -1; ecall"); }
 
 bool ienabled() { return false; }
 
