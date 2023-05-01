338df180c2010cc779630fd310fbeaf8bdb537eb
tracer-ics2022 <tracer@njuics.org>
2023-05-01 18:17:16 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:17:16 up 4 days, 18:27, 13 users,  load average: 0.56, 0.57, 0.46

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -76,7 +76,7 @@ int _read(int fd, void *buf, size_t count) {
   return _syscall_(SYS_read, fd, (intptr_t)buf, count);
 }
 
-int _close(int fd) { return _syscall_(SYS_read, fd, 0, 0); }
+int _close(int fd) { return _syscall_(SYS_close, fd, 0, 0); }
 
 off_t _lseek(int fd, off_t offset, int whence) {
   return _syscall_(SYS_lseek, fd, offset, whence);
