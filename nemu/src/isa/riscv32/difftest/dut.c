c8a9c2d2767f2eae357147373eaa9e5b237ebaea
tracer-ics2022 <tracer@njuics.org>
2023-05-02 02:21:58 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:21:58 up 5 days,  2:32, 16 users,  load average: 0.47, 0.51, 0.50

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -20,7 +20,7 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   bool ans = true;
   if (ref_r->pc != cpu.pc) {
-    printf("pc is 0x%x fact is 0x%x\n", ref_r->pc, cpu.pc);
+    printf("pc expect is 0x%x fact is 0x%x\n", ref_r->pc, cpu.pc);
     ans = false;
   }
   for (int i = 0; i < 32; i = i + 1) {
