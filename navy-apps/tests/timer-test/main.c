23f23d42c6cc582c992bdb27524a848168da7767
tracer-ics2022 <tracer@njuics.org>
2023-05-02 00:53:23 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:53:23 up 5 days,  1:03, 16 users,  load average: 0.47, 0.56, 0.54

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -6,6 +6,7 @@
 const unsigned long Converter = 1000 * 1000; // 1s == 1000 * 1000 us
 
 int main() {
+  NDL_Init(0);
   uint32_t pre = NDL_GetTicks();
   while (1) {
     uint32_t next = NDL_GetTicks();
