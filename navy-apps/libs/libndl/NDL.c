05511fa400ea8639709f43500d478bb67c45fcb8
tracer-ics2022 <tracer@njuics.org>
2023-05-02 18:14:33 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:14:33 up 7 days,  8:28, 10 users,  load average: 0.54, 0.42, 0.33

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -64,6 +64,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   NDL_LoadWH(&pw, &ph); // 获取屏幕的信息
   printf("%d  %d \n", pw, ph);
   uint32_t *next = malloc(pw * ph * sizeof(uint32_t));
+  memset(next, 0, pw * ph * sizeof(uint32_t));
   // 根据相关的pixels和x,y,w,h设置next全部的内容,没有的填充0
   int offset = pw * y + x;
   for (int i = 0; i < h; i++) {
