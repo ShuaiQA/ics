4a0a3a6fc197ebb45dca75cc933b082e4433fe11
tracer-ics2022 <tracer@njuics.org>
2023-05-08 15:50:43 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:50:43 up 13 days,  6:04, 27 users,  load average: 0.79, 0.78, 0.62

--- abstract-machine/am/include/amdev.h
+++ abstract-machine/am/include/amdev.h
@@ -2,6 +2,9 @@
 #define __AMDEV_H__
 
 // **MAY SUBJECT TO CHANGE IN THE FUTURE**
+#include <am.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 #define AM_DEVREG(id, reg, perm, ...)                                          \
   enum { AM_##reg = (id) };                                                    \
@@ -40,17 +43,20 @@ AM_DEVREG(24, NET_RX, WR, Area buf);
 #define AM_KEYS(_)                                                             \
   _(ESCAPE)                                                                    \
   _(F1)                                                                        \
-  _(F2) _(F3) _(F4) _(F5) _(F6) _(F7) _(F8) _(F9) _(F10) _(F11) _(F12)         \
-      _(GRAVE) _(1) _(2) _(3) _(4) _(5) _(6) _(7) _(8) _(9) _(0) _(MINUS)      \
-          _(EQUALS) _(BACKSPACE) _(TAB) _(Q) _(W) _(E) _(R) _(T) _(Y) _(U)     \
-              _(I) _(O) _(P) _(LEFTBRACKET) _(RIGHTBRACKET) _(BACKSLASH)       \
-                  _(CAPSLOCK) _(A) _(S) _(D) _(F) _(G) _(H) _(J) _(K) _(L)     \
-                      _(SEMICOLON) _(APOSTROPHE) _(RETURN) _(LSHIFT) _(Z) _(X) \
-                          _(C) _(V) _(B) _(N) _(M) _(COMMA) _(PERIOD) _(SLASH) \
-                              _(RSHIFT) _(LCTRL) _(APPLICATION) _(LALT)        \
-                                  _(SPACE) _(RALT) _(RCTRL) _(UP) _(DOWN)      \
-                                      _(LEFT) _(RIGHT) _(INSERT) _(DELETE)     \
-                                          _(HOME) _(END) _(PAGEUP) _(PAGEDOWN)
+  _(F2)                                                                        \
+  _(F3)                                                                        \
+  _(F4)                                                                        \
+  _(F5) _(F6) _(F7) _(F8) _(F9) _(F10) _(F11) _(F12) _(GRAVE) _(1) _(2) _(3)   \
+      _(4) _(5) _(6) _(7) _(8) _(9) _(0) _(MINUS) _(EQUALS) _(BACKSPACE)       \
+          _(TAB) _(Q) _(W) _(E) _(R) _(T) _(Y) _(U) _(I) _(O) _(P)             \
+              _(LEFTBRACKET) _(RIGHTBRACKET) _(BACKSLASH) _(CAPSLOCK) _(A)     \
+                  _(S) _(D) _(F) _(G) _(H) _(J) _(K) _(L) _(SEMICOLON)         \
+                      _(APOSTROPHE) _(RETURN) _(LSHIFT) _(Z) _(X) _(C) _(V)    \
+                          _(B) _(N) _(M) _(COMMA) _(PERIOD) _(SLASH) _(RSHIFT) \
+                              _(LCTRL) _(APPLICATION) _(LALT) _(SPACE) _(RALT) \
+                                  _(RCTRL) _(UP) _(DOWN) _(LEFT) _(RIGHT)      \
+                                      _(INSERT) _(DELETE) _(HOME) _(END)       \
+                                          _(PAGEUP) _(PAGEDOWN)
 
 #define AM_KEY_NAMES(key) AM_KEY_##key,
 enum { AM_KEY_NONE = 0, AM_KEYS(AM_KEY_NAMES) };
