3fbc260b255c5d8ed884ca6f87eb852c76220e71
tracer-ics2022 <tracer@njuics.org>
2023-05-10 15:07:18 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:07:18 up  5:01,  5 users,  load average: 0.18, 0.30, 0.28

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -35,7 +35,7 @@ static Finfo file_table[] __attribute__((used)) = {
     [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
     [FD_EVENT] = {"/dev/events", 0, 0, events_read, invalid_write},
     [FD_FB] = {"/dev/fb", 0, 0, invalid_read, fb_write},
-    [FD_DISP] = {"/proc/dispinfo", 8, 0, dispinfo_read, NULL},
+    [FD_DISP] = {"/proc/dispinfo", 64, 0, dispinfo_read, NULL},
 
 #include "files.h"
 };
--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -94,10 +94,9 @@ int NDL_Init(uint32_t flags) {
   dispdev = open("/proc/dispinfo", O_RDONLY);
   fbdev = open("/dev/fb", O_RDONLY);
   evtdev = open("/dev/events", O_RDONLY);
-
   read(dispdev, buf, 64);
   sscanf(buf, "WIDTH: %d\nHEIGHT: %d\n", &screen_w, &screen_h);
-  printf("%d %d\n", screen_w, screen_h);
+  printf("screen_w %d screen_h %d\n", screen_w, screen_h);
   return 0;
 }
 
