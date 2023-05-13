26f09019e118ccd4e1a3ddd83d344fee1f04699c
tracer-ics2022 <tracer@njuics.org>
2023-05-13 17:44:56 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:44:56 up  7:53,  6 users,  load average: 0.43, 0.37, 0.26

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -2,6 +2,9 @@
 #include <unistd.h>
 
 int main() {
+  void *isp;
+  __asm volatile("mov %0, sp" : "=r"(isp) : :);
+  printf("%p", isp);
   write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
