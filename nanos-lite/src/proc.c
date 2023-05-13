9b42a7d46f4a92241cd3649dafcb6a94ea2b64bc
tracer-ics2022 <tracer@njuics.org>
2023-05-13 15:25:23 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:25:23 up  5:33,  5 users,  load average: 0.38, 0.37, 0.67

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -5,10 +5,10 @@
 
 // 记录当前有多少个进程控制块
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
-static PCB pcb_boot = {};
+// static PCB pcb_boot = {};
 PCB *current = NULL;
 
-void switch_boot_pcb() { current = &pcb_boot; }
+void switch_boot_pcb() { current = &pcb[0]; }
 
 void hello_fun(void *arg) {
   int j = 1;
@@ -44,3 +44,12 @@ Context *schedule(Context *prev) {
   current = &pcb[0];
   return current->cp;
 }
+
+// 首先应该分析的是创建一个内核线程需要进行什么初始化操作
+// 在最开始的时候需要确定的是上下文的mepc值,还有就是栈的位置
+// 之后我们分析hello_fun来逐步的进行扩展到其余的情况
+// 当我们初始化之后mepc和sp寄存器之后,函数就会正常执行
+// 之后如果进行yield的时候会发现当前的系统调用会重新保存在PCB中的cp指针中
+// 由schedule可以发现,该cp会指向该进程里的一个新的Context由__am_asm_trap新创建的
+// 之后获取新的pcb数组,然后返回一个Context上下文,执行别的线程流
+// 上面介绍了线程的Context的初始化的过程,以及线程运行中Context的更新过程,选择下一个上下文更新的过程
