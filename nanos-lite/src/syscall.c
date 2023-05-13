0cfd88869dafb93b5c048f527793c43229fd5697
tracer-ics2022 <tracer@njuics.org>
2023-05-13 21:40:57 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:40:57 up 11:49,  6 users,  load average: 0.40, 0.63, 0.59

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -14,13 +14,6 @@ void sys_yield(Context *c) {
   // 我修改了__am_asm_trap汇编代码,先让sp寄存器先加载Context结构体中sp寄存器的位置
   // 然后在根据该位置进行恢复上下文即可完成相关的恢复工作
   c->gpr[2] = (uintptr_t)schedule(c);
-  printf("处理之后\n");
-  printf("%p\n", c);
-  for (int i = 0; i < 32; i++) {
-    printf("%p    ", c->gpr[i]);
-  }
-  printf("%p   ", c->mepc);
-  printf("\n");
 }
 
 void sys_exit(Context *c) { halt(c->GPR2); }
@@ -63,6 +56,13 @@ void do_syscall(Context *c) {
     break;
   case SYS_write:
     c->GPRx = fs_write(c->GPR2, (char *)c->GPR3, c->GPR4);
+    printf("处理之后\n");
+    printf("%p\n", c);
+    for (int i = 0; i < 32; i++) {
+      printf("%p    ", c->gpr[i]);
+    }
+    printf("%p   ", c->mepc);
+    printf("\n");
     break;
   case SYS_brk:
     c->GPRx = (uintptr_t)sys_brk(c->GPR2);
