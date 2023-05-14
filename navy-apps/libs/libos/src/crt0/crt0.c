82dfc4f216251cb75a8ebcbe06482dabab322eb2
tracer-ics2022 <tracer@njuics.org>
2023-05-14 11:58:23 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:58:23 up 1 day,  2:06,  9 users,  load average: 0.38, 0.49, 0.35

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -8,9 +8,9 @@ extern char **environ;
 
 // 当前的args也就是相当于a0
 void call_main(uintptr_t *args) {
-  int pos = *(int *)args;
-  int argc = *((int *)(args) + 1);
-  printf("use pos is %d argc %d\n", pos, argc);
+  uintptr_t pos = *args;
+  uintptr_t argc = *(args + 1);
+  printf("use pos is %ld argc %ld\n", pos, argc);
   char *ags = (char *)args + pos;
   // 对字符串buf进行拆分获取argc,argv
   char *empty[] = {NULL};
