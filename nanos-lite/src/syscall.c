eb3f3aa1d7fbbdba120cdf737262ef3583bdb4d8
tracer-ics2022 <tracer@njuics.org>
2023-05-14 17:18:06 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:18:06 up 1 day,  7:26, 12 users,  load average: 0.72, 0.96, 0.70

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -30,7 +30,6 @@ int sys_gettimeofday(struct timeval *tv, struct timezone *tz) {
 
 int sys_execve(const char *pathname, char *const argv[], char *const envp[]) {
   current->cp = context_uload(current, pathname, argv, envp);
-  switch_boot_pcb();
   yield();
   return 0;
 }
