c1ec5fe286560235ab28f5a1750fa0359d2c09e6
tracer-ics2022 <tracer@njuics.org>
2023-05-14 15:30:52 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:30:52 up 1 day,  5:39, 11 users,  load average: 1.44, 1.47, 0.82

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
@@ -14,6 +15,14 @@ void call_main(uintptr_t *args) {
   printf("use pos is %d argc %d\n", pos, argc);
   char *ags = (char *)args + pos;
   char *argv[argc + 1];
+  for (int i = 0; i < argc; i++) {
+    argv[i] = ags;
+    size_t s = strlen(ags);
+    ags += s;
+  }
+  for (int i = 0; i < argc; i++) {
+    printf("use argv %s\n", argv[i]);
+  }
   // 对字符串buf进行拆分获取argc,argv
   char *empty[] = {NULL};
   environ = empty;
