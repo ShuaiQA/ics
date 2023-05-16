74f5564a2e57f876d55cbdf325657cec79ea146a
tracer-ics2022 <tracer@njuics.org>
2023-05-16 09:59:25 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:59:25 up 16:46,  5 users,  load average: 0.20, 0.15, 0.16

--- abstract-machine/am/src/riscv/ioe/gpu.c
+++ abstract-machine/am/src/riscv/ioe/gpu.c
@@ -23,14 +23,10 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t *src = (uint32_t *)ctl->pixels; // FB_ADDR
   uint32_t *dst = (uint32_t *)FB_ADDR;
-  uint32_t f1 = inl(VGACTL_ADDR);
-  uint32_t heigh = f1 & 0xffff;
-  uint32_t width = f1 >> 16;
-  int cnt = 0;
+  int i = 0;
   for (int j = ctl->y; j < ctl->y + ctl->h; j++) {
-    for (int i = ctl->x; i < ctl->x + ctl->w; i++) {
-      fb[i + j * 400] = p[cnt++];
-    }
+    memcpy(dst + (j * width + ctl->x), src + ctl->w * i, ctl->w);
+    i++;
   }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1); // 直接根据对应的内存地址对nemu的[1]下标进行修改
