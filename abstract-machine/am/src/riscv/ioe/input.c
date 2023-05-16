6e491522b3a3b4c790bf12ab93e4ac1a954ba37a
tracer-ics2022 <tracer@njuics.org>
2023-05-16 10:12:11 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:12:11 up 16:59,  5 users,  load average: 0.23, 0.16, 0.17

--- abstract-machine/am/src/riscv/ioe/input.c
+++ abstract-machine/am/src/riscv/ioe/input.c
@@ -4,8 +4,7 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  uint32_t f1 = inl(KBD_ADDR);
-  int v = (f1 >> 15) & 0x1;
-  kbd->keydown = (v == 0 ? 0 : 1);
-  kbd->keycode = (v == 0 ? f1 : (f1 ^ KEYDOWN_MASK));
+  uint32_t inkey = inl(KBD_ADDR);
+  kbd->keydown = inkey & KEYDOWN_MASK;
+  kbd->keycode = inkey & 0xff;
 }
