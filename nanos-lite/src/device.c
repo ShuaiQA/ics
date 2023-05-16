c403a2084fc2860cd6f02d922a61fc3b71f250fc
tracer-ics2022 <tracer@njuics.org>
2023-05-16 22:00:48 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:00:48 up  2:15,  5 users,  load average: 0.31, 0.28, 0.19

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -16,6 +16,7 @@ static const char *keyname[256]
 
 // 串口写入操作
 size_t serial_write(void *buf, size_t offset, size_t len) {
+  yield();
   char *temp = (char *)buf;
   for (size_t i = 0; i < len; i++) {
     putch(temp[i]);
@@ -25,6 +26,7 @@ size_t serial_write(void *buf, size_t offset, size_t len) {
 
 // 根据keycode获取keyname,按下一次包含输出2次(按下和弹起)
 size_t events_read(void *buf, size_t offset, size_t len) {
+  yield();
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if (ev.keycode == AM_KEY_NONE) {
     return 0;
@@ -39,6 +41,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 
 // 读取屏幕大小信息,根据一定的格式向buf中写入数据
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
+  yield();
   int w = io_read(AM_GPU_CONFIG).width;
   int h = io_read(AM_GPU_CONFIG).height;
   // printf("w is %d h is %d\n", w, h);
@@ -46,12 +49,14 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t sbctl_read(void *buf, size_t offset, size_t len) {
+  yield();
   AM_AUDIO_STATUS_T stat = io_read(AM_AUDIO_STATUS);
   *(int *)buf = stat.count;
   return 4;
 }
 
 size_t fb_write(void *buf, size_t offset, size_t len) {
+  yield();
   int x = offset / 4 % 400;
   int y = offset / 4 / 400;
   io_write(AM_GPU_FBDRAW, x, y, buf, len / 4, 1, true);
@@ -59,12 +64,14 @@ size_t fb_write(void *buf, size_t offset, size_t len) {
 }
 
 size_t sb_write(void *buf, size_t offset, size_t len) {
+  yield();
   Area area = {buf, buf + len};
   io_write(AM_AUDIO_PLAY, area);
   return len;
 }
 
 size_t sbctl_write(void *buf, size_t offset, size_t len) {
+  yield();
   assert(len == 12);
   int *arr = (int *)buf;
   io_write(AM_AUDIO_CTRL, arr[0], arr[1], arr[2]);
