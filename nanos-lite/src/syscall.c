07ab1f29df394451779a8ab9aaca270430334405
tracer-ics2022 <tracer@njuics.org>
2023-05-16 16:24:04 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:24:04 up 23:11,  7 users,  load average: 0.25, 0.19, 0.18

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -37,7 +37,7 @@ void sys_execve(const char *pathname, char *const argv[], char *const envp[]) {
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  // printf("%p\n", c);
+  printf("%p\n", c);
 
   switch (a[0]) {
   case SYS_exit:
