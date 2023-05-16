677a708455d6b77313d05b4637bad44456ce5ab1
zhaoshuai <854901517@qq.com>
2023-05-16 09:52:30 +0800

    NJU-ProjectN/am-kernels ics2021 initialized without tracing
    NJU-ProjectN/am-kernels 71196c72b57d5350db188645b9612d5357c8eaf6 thread-os: fix stack overflow with glibc 2.39

--- abstract-machine/am/src/riscv/ioe/gpu.c
+++ abstract-machine/am/src/riscv/ioe/gpu.c
@@ -1,14 +1,19 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdint.h>
+#include <string.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
-void __am_gpu_init() {}
+static uint32_t width, heigh;
+
+void __am_gpu_init() {
+  uint32_t f1 = inl(VGACTL_ADDR);
+  heigh = f1 & 0xffff;
+  width = f1 >> 16;
+}
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  uint32_t f1 = inl(VGACTL_ADDR);
-  uint32_t heigh = f1 & 0xffff;
-  uint32_t width = f1 >> 16;
   *cfg = (AM_GPU_CONFIG_T){
       .width = width,
       .height = heigh,
@@ -16,15 +21,16 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  uint32_t *p = (uint32_t *)ctl->pixels; // FB_ADDR
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *src = (uint32_t *)ctl->pixels; // FB_ADDR
+  uint32_t *dst = (uint32_t *)FB_ADDR;
+  uint32_t f1 = inl(VGACTL_ADDR);
+  uint32_t heigh = f1 & 0xffff;
+  uint32_t width = f1 >> 16;
   int cnt = 0;
   for (int j = ctl->y; j < ctl->y + ctl->h; j++) {
     for (int i = ctl->x; i < ctl->x + ctl->w; i++) {
       fb[i + j * 400] = p[cnt++];
     }
-  }
-  for (int i = ctl->y; i < ctl->y + ctl->h; i++) {
   }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1); // 直接根据对应的内存地址对nemu的[1]下标进行修改
