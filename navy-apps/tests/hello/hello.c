e33714c113abbaa530f55e30645103b89033d7b3
tracer-ics2022 <tracer@njuics.org>
2023-05-17 10:57:30 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:57:30 up 11:23,  1 user,  load average: 0.58, 0.81, 0.68

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -6,13 +6,13 @@
 extern char _end;
 
 int main(int argc, char *argv[]) {
-  char *m = malloc(10);
-  printf("malloc %p\n", m);
-  printf("malloc begin is %p\n", &_end);
-  // while (1) {
-  //   for (int i = 0; i < argc; i++) {
-  //     printf("app %s\n", argv[i]);
-  //   }
-  // }
+  int i = 0;
+  while (1) {
+    if (i % 2000 == 0) {
+      for (int i = 0; i < argc; i++) {
+        printf("app %s\n", argv[i]);
+      }
+    }
+  }
   return 0;
 }
