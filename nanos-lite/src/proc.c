a8d1ec1eb1bf8801d4b3363a6f3f3087241f7f61
tracer-ics2022 <tracer@njuics.org>
2023-05-17 11:01:42 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:01:42 up 11:27,  1 user,  load average: 0.43, 0.70, 0.68

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -77,7 +77,7 @@ Context *context_uload(PCB *pcb, const char *pathname, char *const argv[],
 void init_proc() {
   context_kload(&pcb[0], hello_fun, NULL);
   // context_kload(&pcb[1], hello_fun, NULL);
-  context_uload(&pcb[1], "/bin/nterm", NULL, NULL);
+  context_uload(&pcb[1], "/bin/nslider", NULL, NULL);
   switch_boot_pcb();
   Log("Initializing processes...");
 
--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -14,6 +14,7 @@ int main(int argc, char *argv[]) {
         printf("app %s\n", argv[i]);
       }
     }
+    i++;
   }
   return 0;
 }
