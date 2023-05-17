957675fc527b2d58527065170c782867401ce6ea
tracer-ics2022 <tracer@njuics.org>
2023-05-17 10:58:23 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:58:23 up 11:24,  1 user,  load average: 0.67, 0.82, 0.69

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -9,6 +9,7 @@ int main(int argc, char *argv[]) {
   int i = 0;
   while (1) {
     if (i % 2000 == 0) {
+      printf("argc %d\n", argc);
       for (int i = 0; i < argc; i++) {
         printf("app %s\n", argv[i]);
       }
