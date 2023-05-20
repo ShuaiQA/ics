bae2f2090b0a08a217c2b686b4c911afbe1b83d0
tracer-ics2022 <tracer@njuics.org>
2023-05-20 20:08:57 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:08:57 up  5:26,  5 users,  load average: 0.64, 0.63, 0.58

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -83,6 +83,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
   while ((next[(uintptr_t)va << 10 >> 22] & 0x1) == 0) { // 设置页表
     assert(((uintptr_t)va << 10 >> 22) < 1024);          // 确保范围是1024
     assert((uintptr_t)pa > 0x80000000);
+    printf("pa is %p\n", pa);
     next[(uintptr_t)va << 10 >> 22] = ((uintptr_t)pa & 0xfffff000) + 0x1;
   }
 }
