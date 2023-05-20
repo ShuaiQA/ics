4455acbd175f3fdace5fb2ff6c54dd75b41c1533
tracer-ics2022 <tracer@njuics.org>
2023-05-20 19:22:30 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:22:30 up  4:39,  5 users,  load average: 0.98, 0.96, 0.67

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -22,15 +22,15 @@ word_t vaddr_ifetch(vaddr_t addr, int len) { return paddr_read(addr, len); }
 
 // 先翻译成物理地址在进行读或者写
 word_t vaddr_read(vaddr_t addr, int len) {
-  Log("read addr is %d\n", addr);
+  Log("read addr is " FMT_PADDR, addr);
   paddr_t paddr = isa_mmu_translate(addr, len, cpu.satp >> 31);
-  Log("p addr is %d\n", paddr);
+  Log("p addr is " FMT_PADDR, paddr);
   return paddr_read(paddr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
-  Log("write addr is %d\n", addr);
+  Log("write addr is " FMT_PADDR, addr);
   paddr_t paddr = isa_mmu_translate(addr, len, cpu.satp >> 31);
-  Log("p addr is %d\n", paddr);
+  Log("p addr is" FMT_PADDR, paddr);
   paddr_write(paddr, len, data);
 }
