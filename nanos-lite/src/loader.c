4ec644c8f9104dfee64d2535ba3a5b7fb7351d6b
tracer-ics2022 <tracer@njuics.org>
2023-05-21 15:41:06 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:41:06 up  5:18,  6 users,  load average: 0.50, 0.33, 0.22

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,9 +1,14 @@
+#include "am.h"
+#include "memory.h"
+#include "nemu.h"
 #include <elf.h>
 #include <fs.h>
 #include <proc.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
 
 #ifdef __LP64__
 #define Elf_Ehdr Elf64_Ehdr
@@ -14,7 +19,7 @@
 #endif
 
 // 根据字符数组获取需要加载到内容中
-uintptr_t load_segement(char *date) {
+uintptr_t load_segement(PCB *pcb, char *date) {
   Elf_Ehdr *hd = (Elf_Ehdr *)date;
   assert(*(uint32_t *)hd->e_ident == 0x464c457f);
   uintptr_t entry = hd->e_entry;
@@ -24,12 +29,39 @@ uintptr_t load_segement(char *date) {
   for (int i = 0; i < hd->e_phnum; i++) {
     Elf_Phdr ph = phs[i];
     if (ph.p_type == PT_LOAD) {
-      Log("%x  %x  %x\n", ph.p_vaddr, ph.p_offset, ph.p_filesz);
-      memmove((void *)ph.p_vaddr, date + ph.p_offset, ph.p_filesz);
-      char *next = (char *)malloc(ph.p_memsz - ph.p_filesz);
-      memset(next, 0, ph.p_memsz - ph.p_filesz);
-      memmove((void *)(ph.p_vaddr + ph.p_filesz), next,
-              ph.p_memsz - ph.p_filesz);
+      Log("%x %x %x %x\n", ph.p_vaddr, ph.p_memsz, ph.p_offset, ph.p_filesz);
+      // 将当前的虚拟地址加载到相关的物理页面中,获取虚拟地址,虚拟地址大小.
+      // 加载数据的位置(文件偏移量),加载数据的大小.
+      uintptr_t vaddr = ph.p_vaddr, vsize = ph.p_memsz, f_offset = ph.p_offset,
+                f_size = ph.p_filesz;
+      uintptr_t size = 0;          // 记录了当前移动了多少的数据
+      char *pos = date + f_offset; // 记录了移动到哪里了
+      while (size + PGSIZE <= f_size) { // 设置p_filesz
+        char *page = new_page(1);
+        memmove(page, pos, PGSIZE);
+        map(&pcb->as, (char *)vaddr, page, 0);
+        pos += PGSIZE;
+        vaddr += PGSIZE; // 虚拟地址自增为了映射
+        size += PGSIZE;
+      }
+      // 设置一个临界的页,也就是f_size剩下的和vsize开始的(如果有的话)
+      if (f_size - size != 0) {
+        char *page = new_page(1);
+        uintptr_t len = f_size - size;
+        // 注意只是移动了len!=4096个,默认new_page的字符是0
+        memmove(page, pos, len);
+        map(&pcb->as, (char *)vaddr, page, 0);
+        pos += PGSIZE;
+        size += PGSIZE;
+        vaddr += PGSIZE;
+      }
+      while (size <= vsize) {
+        char *page = new_page(1); // 默认是0
+        map(&pcb->as, (char *)vaddr, page, 0);
+        pos += PGSIZE;
+        vaddr += PGSIZE; // 虚拟地址自增为了映射
+        size += PGSIZE;
+      }
     }
   }
   return entry;
@@ -42,23 +74,15 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   fs_lseek(fd, 0, SEEK_SET);
   char *date = malloc(size);
   fs_read(fd, date, size);
-  uintptr_t entry = load_segement(date);
+  uintptr_t entry = load_segement(pcb, date);
   fs_close(fd);
+  free(date);
   return entry;
 }
 
 // 加载一个filename的用户程序,返回该用户程序的最初执行的入口
 void *naive_uload(PCB *pcb, const char *filename) {
-  uintptr_t entry;
-  if (filename != NULL) {
-    entry = loader(pcb, filename);
-  } else {
-    size_t size = get_ramdisk_size();
-    Log("ramdisk_read size is %d\n", size);
-    char *buf = malloc(size);
-    ramdisk_read(buf, 0, size);
-    entry = load_segement(buf);
-  }
+  uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", entry);
   return ((void (*)())entry);
 }
