7215023401ad88f25c9cf0f7dfe319e809d10c9c
tracer-ics2022 <tracer@njuics.org>
2023-05-21 17:00:00 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:00:00 up  6:37,  5 users,  load average: 0.39, 0.31, 0.50

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -30,15 +30,15 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   }
   // 查看ab向寄存中写的数据发现需要左移12位找到对应的页目录的地址
   word_t page_dir = cpu.satp << 12; // 找到对应的页目录的物理地址
-  Log("dir is " FMT_WORD, page_dir);
+  // Log("dir is " FMT_WORD, page_dir);
   word_t pte =
       paddr_read(page_dir + (vaddr >> 22) * 4, 4); // 找到页目录中的页表项
   word_t next_page = pte & 0xfffff000; // 根据页表项的获取二级页表的物理地址
-  Log("table is " FMT_WORD, next_page);
+  // Log("table is " FMT_WORD, next_page);
   word_t next_pte = paddr_read(next_page + (vaddr << 10 >> 22) * 4,
                                4); // 获取二级页表中的页表项
   vaddr_t ret = (next_pte & 0xfffff000) + (vaddr & 0xfff);
-  Log("ret is " FMT_WORD, ret);
+  // Log("ret is " FMT_WORD, ret);
   // Assert(vaddr == ret, "vaddr is " FMT_WORD "  cnt is %d\n", vaddr, cnt);
   return ret; // 获取物理地址
 }
