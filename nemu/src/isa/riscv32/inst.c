a8a5303ffa0aabc656285c26c11b50841c785692
tracer-ics2022 <tracer@njuics.org>
2023-05-22 11:24:25 +0800

    >  compile NEMU 121106022677 赵帅 Linux QA 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:24:25 up 1 day,  1:01,  6 users,  load average: 0.55, 0.32, 0.19

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -227,8 +227,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0001100 00000 00000 010 ????? 1110011", csrr_satp, I,
           R(rd) = cpu.satp;);
   INSTPAT("0001100 00000 ????? 001 00000 1110011", csrw_satp, I,
-          cpu.satp = src1;
-          Log("change satp " FMT_WORD, cpu.satp););
+          cpu.satp = src1;);
   INSTPAT("0011000 00010 00000 000 00000 1110011", mret, I,
           s->dnpc = cpu.mepc;);
   INSTPAT("0000000 00001 00000 000 00000 1110011", ebreak, N,
