4e20ed3025d485809f75c34361b4562240b0b7dd
tracer-ics2022 <tracer@njuics.org>
2023-07-15 21:04:15 +0800

    >  compile NEMU 19991122 赵帅 Linux QA 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:04:15 up 1 day, 21:09,  5 users,  load average: 1.15, 1.12, 0.87

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,13 +2,26 @@
 #include "am.h"
 #include "debug.h"
 #include <common.h>
+#include <stdint.h>
+
+uintptr_t sys_yield() { return 0; }
+
+void sys_exit() { halt(0); }
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   switch (a[0]) {
   case SYS_yield:
+    c->GPRx = sys_yield();
     Log("get yield");
+    break;
+  case SYS_exit:
+    sys_exit();
     break;
   default:
     panic("Unhandled syscall ID = %d", a[0]);
