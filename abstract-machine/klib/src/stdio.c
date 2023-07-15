8a40609371949face6fab42e61d108753c281a1f
tracer-ics2022 <tracer@njuics.org>
2023-07-15 10:45:23 +0800

    >  compile NEMU 19991122 赵帅 Linux QA 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:45:23 up 1 day, 10:50,  4 users,  load average: 0.25, 0.28, 0.26

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,28 +1,147 @@
 #include <am.h>
 #include <klib.h>
+
 #include <klib-macros.h>
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+int vsnprintf(char *out, size_t n, const char *s, va_list vl) {
+  int format = 0;
+  int longarg = 0;
+  size_t pos = 0;
+  for (; *s; s++) {
+    if (format) {
+      switch (*s) {
+      case 'l': {
+        longarg = 1;
+        break;
+      }
+      case 'p': {
+        longarg = 1;
+        if (out && pos < n) {
+          out[pos] = '0';
+        }
+        pos++;
+        if (out && pos < n) {
+          out[pos] = 'x';
+        }
+        pos++;
+      }
+      case 'x': {
+        long num = longarg ? va_arg(vl, long) : va_arg(vl, int);
+        int hexdigits = 2 * (longarg ? sizeof(long) : sizeof(int)) - 1;
+        for (int i = hexdigits; i >= 0; i--) {
+          int d = (num >> (4 * i)) & 0xF;
+          if (out && pos < n) {
+            out[pos] = (d < 10 ? '0' + d : 'a' + d - 10);
+          }
+          pos++;
+        }
+        longarg = 0;
+        format = 0;
+        break;
+      }
+      case 'd': {
+        long num = longarg ? va_arg(vl, long) : va_arg(vl, int);
+        if (num < 0) {
+          num = -num;
+          if (out && pos < n) {
+            out[pos] = '-';
+          }
+          pos++;
+        }
+        long digits = 1;
+        for (long nn = num; nn /= 10; digits++)
+          ;
+        for (int i = digits - 1; i >= 0; i--) {
+          if (out && pos + i < n) {
+            out[pos + i] = '0' + (num % 10);
+          }
+          num /= 10;
+        }
+        pos += digits;
+        longarg = 0;
+        format = 0;
+        break;
+      }
+      case 's': {
+        const char *s2 = va_arg(vl, const char *);
+        while (*s2) {
+          if (out && pos < n) {
+            out[pos] = *s2;
+          }
+          pos++;
+          s2++;
+        }
+        longarg = 0;
+        format = 0;
+        break;
+      }
+      case 'c': {
+        if (out && pos < n) {
+          out[pos] = (char)va_arg(vl, int);
+        }
+        pos++;
+        longarg = 0;
+        format = 0;
+        break;
+      }
+      default:
+        break;
+      }
+    } else if (*s == '%') {
+      format = 1;
+    } else {
+      if (out && pos < n) {
+        out[pos] = *s;
+      }
+      pos++;
+    }
+  }
+  if (out && pos < n) {
+    out[pos] = 0;
+  } else if (out && n) {
+    out[n - 1] = 0;
+  }
+  return pos;
+}
+
+char buf[4096];
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  va_list args;
+  va_start(args, fmt);
+  int n = vsnprintf(buf, 4096, fmt, args);
+  if (n == 4096) {
+    putstr("buf is too smail\n");
+  }
+  va_end(args);
+  for (int i = 0; buf[i] != '\0'; i++) {
+    putch(buf[i]);
+  }
+  return n;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  int num = vsnprintf(out, 10240, fmt, ap);
+  return num;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int num = vsnprintf(out, 10240, fmt, ap);
+  va_end(ap);
+  return num;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int num = vsnprintf(out, n, fmt, ap);
+  va_end(ap);
+  return num;
 }
 
 #endif
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,47 +1,97 @@
 #include <klib.h>
+
 #include <klib-macros.h>
 #include <stdint.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  int n;
+
+  for (n = 0; s[n]; n++)
+    ;
+  return n;
 }
 
-char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+char *strcpy(char *s, const char *t) {
+  char *os;
+
+  os = s;
+  while ((*s++ = *t++) != 0)
+    ;
+  return os;
 }
 
-char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+char *strncpy(char *s, const char *t, size_t n) {
+  char *os;
+
+  os = s;
+  while (n-- > 0 && (*s++ = *t++) != 0)
+    ;
+  while (n-- > 0)
+    *s++ = 0;
+  return os;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *os = dst;
+  strcpy(dst + strlen(dst), src);
+  return os;
 }
 
-int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+int strcmp(const char *p, const char *q) {
+  while (*p && *p == *q)
+    p++, q++;
+  return (char)*p - (char)*q;
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+int strncmp(const char *p, const char *q, size_t n) {
+  while (*p && *p == *q && --n)
+    p++, q++;
+  return (char)*p - (char)*q;
 }
 
-void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+void *memset(void *dst, int c, size_t n) {
+  char *cdst = (char *)dst;
+  int i;
+  for (i = 0; i < n; i++) {
+    cdst[i] = c;
+  }
+  return dst;
 }
 
-void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+void *memmove(void *vdst, const void *vsrc, size_t n) {
+  char *dst;
+  const char *src;
+
+  dst = vdst;
+  src = vsrc;
+  if (src > dst) {
+    while (n-- > 0)
+      *dst++ = *src++;
+  } else {
+    dst += n;
+    src += n;
+    while (n-- > 0)
+      *--dst = *--src;
+  }
+  return vdst;
 }
 
-void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+void *memcpy(void *dst, const void *src, size_t n) {
+  return memmove(dst, src, n);
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const char *p1 = s1, *p2 = s2;
+  while (n-- > 0) {
+    if (*p1 != *p2) {
+      return *p1 - *p2;
+    }
+    p1++;
+    p2++;
+  }
+  return 0;
 }
 
 #endif
