7c45d72044ec43219938d3ed66b2238522c59fc5
tracer-ics2022 <tracer@njuics.org>
2023-07-15 15:46:51 +0800

    >  compile NEMU 19991122 赵帅 Linux QA 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:46:51 up 1 day, 15:51,  5 users,  load average: 0.30, 0.26, 0.27

--- nanos-lite/include/common.h
+++ nanos-lite/include/common.h
@@ -2,14 +2,15 @@
 #define __COMMON_H__
 
 /* Uncomment these macros to enable corresponding functionality. */
-//#define HAS_CTE
+#define HAS_CTE
 //#define HAS_VME
 //#define MULTIPROGRAM
 //#define TIME_SHARING
 
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
+
 #include <debug.h>
 
 #endif
