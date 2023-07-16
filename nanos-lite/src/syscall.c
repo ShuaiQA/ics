a3f26f2326621c0d1792c4442dcd51995db78dae
tracer-ics2022 <tracer@njuics.org>
2023-07-16 10:30:22 +0800

    >  compile NEMU 19991122 赵帅 Linux QA 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:30:22 up 2 days, 10:35, 10 users,  load average: 0.33, 0.28, 0.22

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -34,7 +34,6 @@ void do_syscall(Context *c) {
     break;
   case SYS_lseek:
     c->GPRx = fs_lseek(a[1], a[2], a[3]);
-    Log("%p", c->GPRx);
     break;
   case SYS_write:
     c->GPRx = fs_write(a[1], (void *)a[2], a[3]);
