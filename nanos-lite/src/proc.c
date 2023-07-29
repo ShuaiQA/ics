a62d62b0ced60553acaf1a288ef5e52c7caf7a78
tracer-ics2022 <tracer@njuics.org>
2023-07-29 10:30:23 +0800

    >  compile NEMU 19991122 赵帅 Linux QA 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:30:23 up 2 days, 13:15, 14 users,  load average: 0.06, 0.17, 0.17

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -62,7 +62,8 @@ Context *context_uload(PCB *pcb, const char *pathname, char *const argv[],
 
 void init_proc() {
   context_kload(&pcb[0], hello_fun, (void *)10);
-  context_uload(&pcb[1], "/bin/hello", NULL, NULL);
+  context_kload(&pcb[1], hello_fun, (void *)20);
+  // context_uload(&pcb[1], "/bin/hello", NULL, NULL);
   switch_boot_pcb();
   Log("Initializing processes...");
   // naive_uload(NULL, "/bin/nterm");
@@ -74,6 +75,7 @@ Context *schedule(Context *prev) {
   // 将当前的上下文保存到current指向的pcb数组下标中
   current->cp = prev;
   current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+
   // current = &pcb[0];
   return current->cp;
 }
