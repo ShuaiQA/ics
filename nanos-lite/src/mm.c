242c54e5ab2b3d28d9d93c97bc710499cbd778bd
tracer-ics2022 <tracer@njuics.org>
2023-07-29 20:55:26 +0800

    >  compile NEMU 19991122 赵帅 Linux QA 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:55:26 up  8:11,  4 users,  load average: 0.61, 0.68, 1.03

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -1,27 +1,27 @@
+#include "klib-macros.h"
 #include <memory.h>
 
 static void *pf = NULL;
 
-void* new_page(size_t nr_page) {
-  return NULL;
+// 分配多少的页面数目
+void *new_page(size_t nr_page) {
+  void *ret = pf;
+  pf += PGSIZE * nr_page;
+  return ret;
 }
 
 #ifdef HAS_VME
-static void* pg_alloc(int n) {
-  return NULL;
-}
+// 分配多少字节数目
+static void *pg_alloc(int n) { return new_page(ROUNDUP(n, PGSIZE)); }
 #endif
 
-void free_page(void *p) {
-  panic("not implement yet");
-}
+void free_page(void *p) { panic("not implement yet"); }
 
 /* The brk() system call handler. */
-int mm_brk(uintptr_t brk) {
-  return 0;
-}
+int mm_brk(uintptr_t brk) { return 0; }
 
 void init_mm() {
+  // 获取页面的初始化的地址
   pf = (void *)ROUNDUP(heap.start, PGSIZE);
   Log("free physical pages starting from %p", pf);
 
