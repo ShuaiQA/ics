be211e967fe123a5f4b7dd37da799231445366c3
tracer-ics2022 <tracer@njuics.org>
2023-07-29 17:01:13 +0800

    >  compile NEMU 19991122 赵帅 Linux QA 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:01:13 up 2 days, 19:46, 14 users,  load average: 0.31, 0.15, 0.15

--- nemu/src/monitor/sdb/etrace.c
+++ nemu/src/monitor/sdb/etrace.c
@@ -9,50 +9,29 @@ typedef struct etrace {
   word_t a0, a1, a2;
   int syscall;
   word_t ret;
-  struct etrace *next; // 记录嵌套的层数
 } etrace;
 
 #define SIZE_ETR_BUF 16
-static etrace buf[SIZE_ETR_BUF];
+#define DEP 5
+static etrace buf[SIZE_ETR_BUF][DEP];
 static int step[SIZE_ETR_BUF]; // 记录每一个ecall嵌套的层数
+static int max_step[SIZE_ETR_BUF];
 static int pos = 0;
 
 void new_etrace() {
-  if (step[pos] >= 1) {
-    etrace *cur = malloc(sizeof(etrace));
-    cur->a0 = cpu.gpr[10];
-    cur->a1 = cpu.gpr[11];
-    cur->a2 = cpu.gpr[12];
-    cur->syscall = cpu.gpr[17];
-    etrace *last = &buf[pos];
-    while (last->next != NULL) {
-      last = last->next;
-    }
-    last->next = cur;
-  } else {
-    buf[pos].a0 = cpu.gpr[10];
-    buf[pos].a1 = cpu.gpr[11];
-    buf[pos].a2 = cpu.gpr[12];
-    buf[pos].syscall = cpu.gpr[17];
-  }
+  buf[pos][step[pos]].a0 = cpu.gpr[10];
+  buf[pos][step[pos]].a1 = cpu.gpr[11];
+  buf[pos][step[pos]].a2 = cpu.gpr[12];
+  buf[pos][step[pos]].syscall = cpu.gpr[17];
   step[pos]++;
+  max_step[pos]++;
 }
 
 void new_ret() {
-  int cur_step = step[pos];
-  if (cur_step == 1) {
-    buf[pos].ret = cpu.gpr[10];
-    step[pos]--;
-    assert(step[pos] == 0); // 跳转到下一个则上一个必定完毕
+  step[pos]--;
+  buf[pos][step[pos]].ret = cpu.gpr[10];
+  if (step[pos] == 0) {
     pos = (pos + 1) % SIZE_ETR_BUF;
-  } else {
-    etrace *p = &buf[pos];
-    while (cur_step > 1) {
-      p = p->next;
-      cur_step--;
-    }
-    p->ret = cpu.gpr[10];
-    step[pos]--;
   }
 }
 
@@ -60,17 +39,14 @@ void new_ret() {
 void print_etrace() {
   int i = pos;
   do {
-    etrace *cur = &buf[i];
-    int step = 0;
-    while (cur != NULL) {
-      for (int i = 0; i < step; i++) {
+    for (int j = 0; j < max_step[i]; j++) {
+      for (int k = 0; k < j; k++) {
         printf("  ");
       }
       printf("syscall %d  parameter " FMT_WORD " " FMT_WORD "  " FMT_WORD
              " ret " FMT_WORD " \n",
-             cur->syscall, cur->a0, cur->a1, cur->a2, cur->ret);
-      cur = cur->next;
-      step++;
+             buf[i][j].syscall, buf[i][j].a0, buf[i][j].a1, buf[i][j].a2,
+             buf[i][j].ret);
     }
     i = (i + 1) % SIZE_ETR_BUF;
   } while (i != pos);
