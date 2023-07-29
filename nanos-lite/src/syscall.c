e0861849448a5cb29790703bfeb5436e25c87f28
tracer-ics2022 <tracer@njuics.org>
2023-07-29 18:35:16 +0800

    >  compile NEMU 19991122 赵帅 Linux QA 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:35:16 up 2 days, 21:20, 15 users,  load average: 0.38, 0.55, 0.61

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -18,7 +18,9 @@ int sys_gettimeofday(struct timeval *tv, struct timezone *tz) {
   return 0;
 }
 
-void sys_exit() { naive_uload(NULL, "/bin/nterm"); }
+uintptr_t sys_exit() {
+  return (uintptr_t)context_uload(current, "/bin/nterm", NULL, NULL);
+}
 
 uintptr_t sys_brk(uintptr_t size) { return 0; }
 
@@ -35,14 +37,14 @@ void do_syscall(Context *c) {
   a[3] = c->GPR4;
 
   switch (a[0]) {
+  case SYS_exit:
+    c->next_context = sys_exit();
+    break;
   case SYS_yield:
     sys_yield(c);
     break;
   case SYS_open:
     c->GPRx = fs_open((char *)a[1], a[2], a[3]);
-    break;
-  case SYS_exit:
-    sys_exit();
     break;
   case SYS_read:
     c->GPRx = fs_read(a[1], (char *)a[2], a[3]);
@@ -68,7 +70,7 @@ void do_syscall(Context *c) {
   default:
     panic("Unhandled syscall ID = %d", a[0]);
   }
-  if (a[0] != SYS_yield && a[0] != SYS_execve) {
+  if (a[0] != SYS_yield && a[0] != SYS_execve && a[0] != SYS_exit) {
     c->next_context = (uintptr_t)c;
   }
 }
