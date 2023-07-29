3c3bf588a3226f87ccac6767c644872738e1af37
tracer-ics2022 <tracer@njuics.org>
2023-07-29 18:10:52 +0800

    >  compile NEMU 19991122 赵帅 Linux QA 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:10:52 up 2 days, 20:56, 15 users,  load average: 0.52, 0.25, 0.42

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -72,7 +72,7 @@ Context *context_uload(PCB *pcb, const char *pathname, char *const argv[],
 
 void init_proc() {
   // context_kload(&pcb[0], hello_fun, (void *)10);
-  // context_kload(&pcb[1], hello_fun, (void *)20);
+  context_kload(&pcb[1], hello_fun, (void *)20);
   context_uload(&pcb[0], "/bin/hello", NULL, NULL);
   switch_boot_pcb();
   Log("Initializing processes...");
