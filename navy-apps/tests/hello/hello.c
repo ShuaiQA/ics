7bb96ac926e5472d027dd5438c665f175ca3643f
tracer-ics2022 <tracer@njuics.org>
2023-08-01 15:14:53 +0800

    >  compile NEMU 19991122 赵帅 Linux ubuntu-linux-22-04-desktop 5.15.0-78-generic #85-Ubuntu SMP Fri Jul 7 15:29:30 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  15:14:53 up  6:07,  1 user,  load average: 0.34, 0.24, 0.19

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -4,10 +4,11 @@
 int main() {
   write(1, "Hello World!\n", 13);
   int i = 2;
-  while(1)
+  while(1){
     if (i % 10 == 0) {
       printf("Hello World from Navy-apps for the %dth time!\n", i++);
     }
     i++;
+  }
   _exit(0);
 }
