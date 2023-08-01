c48519bc8529113022b0c3e8338c282ec2ef35fc
tracer-ics2022 <tracer@njuics.org>
2023-08-01 18:27:04 +0800

    >  compile NEMU 19991122 赵帅 Linux ubuntu-linux-22-04-desktop 5.15.0-78-generic #85-Ubuntu SMP Fri Jul 7 15:29:30 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  18:27:04 up  8:21,  1 user,  load average: 0.14, 0.17, 0.12

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -179,13 +179,14 @@ static int cmd_info(char *args) {
 #define CONTEXT_STACK 8*4096
 static int cmd_pp(char *args){
   char *token = strtok(args, " ");
-  word_t cnt;
+  word_t ctx;
   if (token != NULL) {
-    sscanf(token, "%ld", &cnt);
+    sscanf(token, "%ld", &ctx);
   }
   // 获取当前的进程的cte指针的地址
-  Log("cnt is %ld ,"FMT_WORD  ,cnt,CONTEXT_BASE + cnt*CONTEXT_STACK);
-  word_t ctx_addr = paddr_read(CONTEXT_BASE + cnt * CONTEXT_STACK , 8);
+  ctx = CONTEXT_BASE + ctx * CONTEXT_STACK;
+  Log(FMT_WORD ,ctx );
+  word_t ctx_addr = paddr_read(ctx , 8);
   Log("ctx addr is "FMT_WORD  ,ctx_addr);
   for(int i=0;i<36;i++){
     word_t reg = paddr_read(ctx_addr + i * 8, 8);
