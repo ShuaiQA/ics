9f214996d9ec4a6ed8768f2a2eb01cc5c2033c5b
tracer-ics2022 <tracer@njuics.org>
2023-08-01 12:30:29 +0800

    >  compile NEMU 19991122 赵帅 Linux ubuntu-linux-22-04-desktop 5.15.0-78-generic #85-Ubuntu SMP Fri Jul 7 15:29:30 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  12:30:29 up  4:40,  1 user,  load average: 0.05, 0.12, 0.15

--- navy-apps/scripts/riscv64.mk
+++ navy-apps/scripts/riscv64.mk
@@ -1,7 +1,7 @@
 CROSS_COMPILE = riscv64-linux-gnu-
 LNK_ADDR = $(if $(VME), 0x40000000, 0x83000000)
 CFLAGS  += -fno-pic -march=rv64g -mcmodel=medany
-LDFLAGS += --no-relax -Ttext-segment $(LNK_ADDR)
+# LDFLAGS += --no-relax -Ttext-segment $(LNK_ADDR)
 ifdef VME
 	LDFLAGS += --no-relax -T $(NAVY_HOME)/scripts/user.ld
 else
