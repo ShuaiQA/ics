d9fc8b6b0038f1f2a5b2e615687c94207aace2c9
tracer-ics2022 <tracer@njuics.org>
2023-08-01 15:08:18 +0800

    >  compile NEMU 19991122 赵帅 Linux ubuntu-linux-22-04-desktop 5.15.0-78-generic #85-Ubuntu SMP Fri Jul 7 15:29:30 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  15:08:18 up  6:00,  1 user,  load average: 0.38, 0.29, 0.21

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -72,9 +72,9 @@ enum {
 
 // imm -> rd , rs1 -> imm
 void csrrw(word_t imm, word_t src1, word_t rd) {
-//   if (imm == satp) {
-//     Log(" write satp " FMT_WORD, src1);
-//   }
+  if (imm == satp) {
+    Log(" write satp " FMT_WORD, src1);
+  }
   if (rd != 0) {
     R(rd) = RM(imm);
   }
