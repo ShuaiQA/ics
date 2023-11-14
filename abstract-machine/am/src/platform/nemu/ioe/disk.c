160192ca8cb0f30edcf68bc0b4cc2f40f3158f8a
tracer-ics2022 <tracer@njuics.org>
2023-11-14 19:33:38 +0800

    >  compile NEMU 19991122 赵帅 Linux QA 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:33:38 up 2 days, 8 min,  4 users,  load average: 0.18, 0.17, 0.24

--- abstract-machine/am/src/platform/nemu/ioe/disk.c
+++ abstract-machine/am/src/platform/nemu/ioe/disk.c
@@ -1,12 +1,9 @@
 #include <am.h>
 #include <nemu.h>
 
-void __am_disk_config(AM_DISK_CONFIG_T *cfg) {
-  cfg->present = false;
-}
+// 初始化disk是否虚拟化以及有多少块和每块的大小
+void __am_disk_config(AM_DISK_CONFIG_T *cfg) { cfg->present = false; }
 
-void __am_disk_status(AM_DISK_STATUS_T *stat) {
-}
+void __am_disk_status(AM_DISK_STATUS_T *stat) {}
 
-void __am_disk_blkio(AM_DISK_BLKIO_T *io) {
-}
+void __am_disk_blkio(AM_DISK_BLKIO_T *io) {}
