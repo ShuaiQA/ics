b1be03ef169ca0cb0705cc2b46e738a0d3d4f450
tracer-ics2022 <tracer@njuics.org>
2023-11-17 20:13:06 +0800

    >  compile NEMU 19991122 赵帅 Linux QA 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:13:06 up 1 day,  7:35,  5 users,  load average: 0.47, 0.55, 0.49

--- nemu/src/device/disk.c
+++ nemu/src/device/disk.c
@@ -78,7 +78,7 @@ void init_disk() {
 
 void set_disk_file(char *disk) {
   fd = open(disk, O_RDWR);
-  Assert(fd <= 3, "open fail");
+  Assert(fd > 3, "open fail");
   char buf[512];
   int len = read(fd, buf, 512);
   Log("nemu %d %s  %d\n", fd, buf, len);
