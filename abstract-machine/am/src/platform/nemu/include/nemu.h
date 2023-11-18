1a7d75939bcde991ff9584f2bfb26eed1bee3f9a
tracer-ics2022 <tracer@njuics.org>
2023-11-18 09:14:10 +0800

    >  compile NEMU 19991122 赵帅 Linux QA 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:14:10 up 27 min,  5 users,  load average: 0.09, 0.19, 0.22

--- abstract-machine/am/src/platform/nemu/include/nemu.h
+++ abstract-machine/am/src/platform/nemu/include/nemu.h
@@ -44,6 +44,7 @@ extern char _pmem_start;
 #define NEMU_PADDR_SPACE                                                       \
   RANGE(&_pmem_start, PMEM_END), RANGE(FB_ADDR, FB_ADDR + 0x200000),           \
       RANGE(MMIO_BASE, MMIO_BASE + 0x1000),                                    \
+      RANGE(DISK_BUF_ADDR, DISK_BUF_ADDR + 0x1000)
 
 typedef uintptr_t PTE;
 #define PXMASK 0x1FF // 9 bits
