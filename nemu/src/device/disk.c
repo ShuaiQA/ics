5e4357a3287a819508f6e2ef09c98c3ad958ebaa
tracer-ics2022 <tracer@njuics.org>
2023-11-18 09:06:03 +0800

    >  compile NEMU 19991122 赵帅 Linux QA 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:06:03 up 19 min,  5 users,  load average: 0.22, 0.43, 0.28

--- nemu/src/device/disk.c
+++ nemu/src/device/disk.c
@@ -40,15 +40,15 @@ uint8_t *block;
 static void disk_io_handler(uint32_t offset, int len, bool is_write) {
   // 等待中且对于config有操作的时候才执行宿主机处理
   if (offset == config * 4 && disk_base[config] == 1) {
-    if (disk_base[rw] == 0) { // 磁盘写操作
-      lseek(fd, CONFIG_DISK_BLOCK_SIZE * disk_base[no], SEEK_SET);
-      int len = write(fd, block, CONFIG_DISK_BLOCK_SIZE);
-      Assert(len != CONFIG_DISK_BLOCK_SIZE, "写入磁盘失败");
-    } else { // 读取磁盘的数据
+    if (disk_base[rw] == 0) { // 磁盘读操作
       Log("duqucaozuo ");
       lseek(fd, CONFIG_DISK_BLOCK_SIZE * disk_base[no], SEEK_SET);
       int len = read(fd, block, CONFIG_DISK_BLOCK_SIZE);
       Assert(len != CONFIG_DISK_BLOCK_SIZE, "读取磁盘失败");
+    } else {
+      lseek(fd, CONFIG_DISK_BLOCK_SIZE * disk_base[no], SEEK_SET);
+      int len = write(fd, block, CONFIG_DISK_BLOCK_SIZE);
+      Assert(len != CONFIG_DISK_BLOCK_SIZE, "写入磁盘失败");
     }
   }
   if (offset == no * 4) {
